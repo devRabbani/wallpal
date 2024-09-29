8ac3a80d3a2f1b99d0403ffcda127165eab3b7c8
Golam Rabbani <dev.golamrabbani@gmail.com>
2024-09-29 20:02:54 +0530

    add meta data and icons

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,6 +1,14 @@
 import Gallery from "@/components/gallery-page";
 import { getWallpapers } from "@/lib/actions";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Gallery - Wallpal",
+  description:
+    "Browse through a collection of stunning wallpapers created with Wallpal. Get inspired and create your own unique designs.",
+};
+
 export default async function GalleryPage() {
   const wallpaperRes = await getWallpapers();
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,69 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/nav";
 import { Toaster } from "@/components/ui/sonner";
 import NextTopLoader from "nextjs-toploader";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
-  title: "Wallpal",
-  description: "Wallpaper Generator with Typescript",
+  title: "Wallpal - Transform Your Screen in Seconds!",
+  description:
+    "Create unique, personalized wallpapers for your devices with Wallpal. Customize colors, patterns, and text to match your style.",
+  keywords: [
+    "wallpaper",
+    "generator",
+    "custom",
+    "design",
+    "personalized",
+    "Wallpal",
+  ],
+  authors: [{ name: "Golam Rabbani" }, { name: "Kaushik Das" }],
+  creator: "CanWeBe!",
+  publisher: "CanWeBe!",
+  openGraph: {
+    title: "Wallpal - Transform Your Screen in Seconds!",
+    description:
+      "Create unique, personalized wallpapers for your devices with Wallpal.",
+    url: "https://wallpal.vercel.app",
+    siteName: "Wallpal",
+    images: [
+      {
+        url: "https://wallpal.vercel.app/ogimage.png",
+        width: 1200,
+        height: 630,
+        alt: "Wallpal - Transform Your Screen in Seconds Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wallpal - Transform Your Screen in Seconds!",
+    description:
+      "Create unique, personalized wallpapers for your devices with Wallpal.",
+    creator: "@wallpal",
+    images: ["https://wallpal.vercel.app/ogimage.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: "76C2hWhD9MhyJAHhDRqU-1zoKf3sK3pSFZxhqnXmRo4",
+  },
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -23,7 +72,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable}  antialiased`}>
+      <body className={`${inter.className} antialiased`}>
         <main className="container">
           <Nav />
           {children}
